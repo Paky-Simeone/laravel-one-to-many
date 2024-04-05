1102980874a47524d6c376db3a3c210d17958b9f
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-04-05 16:28:32 +0200

    fixed

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,10 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
+// use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -17,11 +18,16 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $types_id = Type::all()->pluck('id');
+        
+
         for($i=0; $i<150; $i++ ){
             $project = new Project;
             $project->title = $faker->catchPhrase();
-            $project->content = $faker->paragraphs(2, true);
-            $project->slug = Str::slug($project->title);
+            $project->type_id = $faker->randomElement($types_id);
+            $project->author = $faker->word();
+            $project->description = $faker->catchPhrase();
+            $project->project_link = $faker->catchPhrase();
             $project->save();
         }
         
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Faker\Generator as Faker;
 
 class TypeSeeder extends Seeder
 {
@@ -12,8 +14,17 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $types_name = ['FrontEnd', 'BackEnd', 'Fullstack'];
+
+        foreach ($types_name as $type_name){
+            $type = new Type;
+            $type->label = $type_name;
+            $type->color = $faker->hexColor();
+            $type->save();
+
+        }
+
     }
 }
